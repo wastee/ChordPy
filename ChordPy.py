ee0a4dca14eb33cd8959ff6e3ceabfeaa146f7af
tkoft <gary.chen@williams.edu>
2016-04-10 15:24:14 -0400

    Multiple transpositions at once

--- ChordPy.py
+++ ChordPy.py
@@ -14,49 +14,51 @@ root.withdraw()
 filename = filedialog.askopenfilename(title="ChordPy: Chose ChordPro .txt File")
 if filename != "":
 	while True:	
-		trans = simpledialog.askstring("ChordPy", "Transpose (+/- #): ")
-		if trans == "" or trans == None:
-			trans = 0
+		transText = simpledialog.askstring("ChordPy", "Transpose (+/- #s, comma separated): ")
+		if transText == "" or transText == None:
+			transText = 0
 			break	
 		else:
 			try:		
-				trans = int(trans)%12
+				transValues = transText.split(",")
+				transValues = list(map(int, transValues))
 				break
 			except ValueError:
 				messagebox.showinfo(title="ChordPy", message="Invalid transposition.")
 				#print("Invalid transposition.")		
 
-	f = open(filename, "r")
-	outFilename = filename[:len(filename)-4]+"_transposed"+str(trans)+".txt"
-	out = open(outFilename, "w+")
+	for trans in transValues:
+		f = open(filename, "r")
+		outFilename = filename[:len(filename)-4]+"_transposed"+str(trans)+".txt"
+		out = open(outFilename, "w+")
 
-	for line in f:
-		outputChords = ""
-		outputLyrics = ""
-		inBracket = False
-		for i in range(len(line)):
-			if line[i] == '[':
-				inBracket = True
-				outputChords = outputChords + " "*(max(1, len(outputLyrics)-len(outputChords)))
-			elif inBracket and line[i] == ']':
-				inBracket = False
-			elif inBracket and line[i] =='#':
-				pass
-			elif inBracket and str.isupper(line[i]):
-				if line[i+1] == '#':
-					outputChords += transpose(line[i]+"#", trans)
+		for line in f:
+			outputChords = ""
+			outputLyrics = ""
+			inBracket = False
+			for i in range(len(line)):
+				if line[i] == '[':
+					inBracket = True
+					outputChords = outputChords + " "*(max(1, len(outputLyrics)-len(outputChords)))
+				elif inBracket and line[i] == ']':
+					inBracket = False
+				elif inBracket and line[i] =='#':
+					pass
+				elif inBracket and str.isupper(line[i]):
+					if line[i+1] == '#':
+						outputChords += transpose(line[i]+"#", trans)
+					else:
+						outputChords += transpose(line[i], trans)
+				elif inBracket:
+					#DONT TRANSPOSE AND PUT IN OUTPUTCHORDS
+					outputChords = outputChords + line[i]
 				else:
-					outputChords += transpose(line[i], trans)
-			elif inBracket:
-				#DONT TRANSPOSE AND PUT IN OUTPUTCHORDS
-				outputChords = outputChords + line[i]
-			else:
-				outputLyrics = outputLyrics + line[i]
-		if not str.strip(outputChords) == "":
-			out.write(outputChords+"\n")
-		out.write(outputLyrics)
+					outputLyrics = outputLyrics + line[i]
+			if not str.strip(outputChords) == "":
+				out.write(outputChords+"\n")
+			out.write(outputLyrics)
 
-	f.close()
-	out.close()
+		f.close()
+		out.close()
 
-	messagebox.showinfo(title="ChordPy", message=("Transposed " + str(trans) + " steps to " + outFilename))
+	messagebox.showinfo(title="ChordPy", message=("Transposed " + ", ".join(map(str, transValues)) + " steps."))
